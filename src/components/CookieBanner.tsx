281da63fff011083cadb530db9d78e29c97bff3e
RubenCM03 <rubencordero003@gmail.com>
2025-07-22 12:00:13 +0200

    Cookies colors corrections and translations

--- src/components/CookieBanner.tsx
+++ src/components/CookieBanner.tsx
@@ -1,7 +1,14 @@
 import { useEffect, useState } from 'react';
 import Cookies from 'js-cookie';
+import { getI18N } from "@/i18n/index.ts";
 
-export default function CookieBanner() {
+type CookieBannerProps = {
+  currentLocale: "de" | "en" | "fr" | string;
+};
+
+export default function CookieBanner({ currentLocale }: CookieBannerProps) {
+  const i18n = getI18N({ currentLocale });
+
   const [visible, setVisible] = useState(false);
 
   const path = typeof window !== 'undefined' ? window.location.pathname : '/';
@@ -56,27 +63,27 @@ export default function CookieBanner() {
   if (!visible) return null;
 
   return (
-    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/60 backdrop-blur-sm">
-      <div className="bg-[#193366] max-w-md w-full p-6 rounded-xl shadow-lg text-center mx-4">
-        <h2 className="text-xl font-semibold mb-4">Zustimmung zu Cookies</h2>
+    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/40 backdrop-blur-sm">
+      <div className="bg-white/10 max-w-md w-full p-6 rounded-xl shadow-lg text-center mx-4">
+        <h2 className="text-xl font-bold mb-3">{i18n.cookieBanner.TITLE}</h2>
         <p className="text-sm mb-2">
-          Wir verwenden Cookies, um sicherzustellen, dass Sie die beste Erfahrung auf unserer Website machen.
+          {i18n.cookieBanner.TEXT_1}
         </p>
-        <p className="text-sm mb-6">
-          Lesen Sie unsere <a href={cookiesPage} className='text-blue-700 hover:text-blue-800 underline'>Datenschutz-Bestimmungen</a>
+        <p className="text-sm mb-4">
+          {i18n.cookieBanner.TEXT_2} <a href={cookiesPage} className='text-[#0C3956] hover:text-[#151f2b] underline'>{i18n.cookieBanner.TEXT_3}</a>
         </p>
         <div className="flex justify-center gap-4">
           <button
             onClick={acceptCookies}
-            className="px-4 py-2 rounded cursor-pointer bg-[#00204b] hover:bg-[#001735] text-white font-semibold transition-all duration-300"
+            className="px-4 py-2 rounded cursor-pointer bg-[#0C3956] hover:bg-[#061B29] text-white font-semibold transition-all duration-300"
           >
-            Akzeptieren
+            {i18n.cookieBanner.BUTTON_1}
           </button>
           <button
             onClick={rejectCookies}
             className="px-4 py-2 rounded cursor-pointer bg-gray-300 hover:bg-gray-400 text-gray-800 font-semibold transition-all duration-300"
           >
-            Ablehnen
+            {i18n.cookieBanner.BUTTON_2}
           </button>
         </div>
       </div>
